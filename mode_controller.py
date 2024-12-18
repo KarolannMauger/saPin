435399bfebbff66881ae0f014906f8820885b3d0
KarolannMauger <karolannmauger@gmail.com>
2024-12-18 11:45:42 -0500

    Clean up mode_controller.py

--- mode_controller.py
+++ mode_controller.py
@@ -1,21 +1,22 @@
 import tcp_connection
 import principal_thread
 import jinglebell
-import TEMT6000_sensor_NeedTCPCall
+import TEMT6000_sensor
 from threading import Thread
 
+# Les print en commentaire c'était pour les test si vous souhaitez voir dans le terminal qu'est-ce que le script envoie
 
 def mode1():
     tcp_connection.send_tcp_message("stop") 
-    print("main def mode1 stop")
-    principal_thread.stop_sensor()
+    #print("main def mode1 stop")
+    principal_thread.stop_principal_thread()
     principal_thread.restart_principal_thread(jinglebell.start)  
     tcp_connection.send_tcp_message("mode1")
-    print("main def mode1 mode1")
+    #print("main def mode1 mode1")
 
 
 def mode2():
     tcp_connection.send_tcp_message("stop")
-    print("main def mode2 stop")
-    principal_thread.stop_sensor() 
-    principal_thread.restart_principal_thread(TEMT6000_sensor_NeedTCPCall.TEMT6000_sensor)
+    #print("main def mode2 stop")
+    principal_thread.stop_principal_thread() 
+    principal_thread.restart_principal_thread(TEMT6000_sensor.start)
